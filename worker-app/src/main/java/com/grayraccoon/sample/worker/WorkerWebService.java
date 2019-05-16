0e1617f3b86744b1c686b85ded23b4f5a1f24bdd
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2019-05-15 23:36:14 -0500

    Adding logging configuration to Worker-App

--- worker-app/src/main/java/com/grayraccoon/sample/worker/WorkerWebService.java
+++ worker-app/src/main/java/com/grayraccoon/sample/worker/WorkerWebService.java
@@ -1,6 +1,9 @@
 package com.grayraccoon.sample.worker;
 
 import lombok.Data;
+import lombok.ToString;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +15,21 @@ import java.net.UnknownHostException;
 @RequestMapping("/ws")
 public class WorkerWebService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkerWebService.class);
+
     @GetMapping("/info")
     public BasicInfo getBasicInfo() throws UnknownHostException {
+        LOGGER.info("Getting basic Info.");
         final InetAddress ip = InetAddress.getLocalHost();
         final BasicInfo basicInfo = new BasicInfo();
         basicInfo.setAddress( ip.toString() );
         basicInfo.setHostname( ip.getHostName() );
+        LOGGER.info("Returning basic info: {}", basicInfo);
         return basicInfo;
     }
 
     @Data
+    @ToString
     private static class BasicInfo {
         private String address;
         private String hostname;
